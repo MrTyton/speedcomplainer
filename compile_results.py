0a915adc918a619edb9b5e05aee102af1c33b748
MrTyton <joshua.gang@gmail.com>
2017-09-18 15:32:29 -0400

    pep8 formatting and removal of extraneous imports.

--- compile_results.py
+++ compile_results.py
@@ -17,28 +17,46 @@ def get_results(date=1):
             if row['Date'] == yester:
                 results.append(row)
     return results
-        
-        
+
+
 def analysis(data, field):
     if field not in data[0].keys():
         raise Exception("Fieldname not found.")
     datums = [float(x[field]) for x in data]
-    return {'min': min(datums), 'max': max(datums), 'mean': "{0:.2f}".format(sum(datums) / len(datums))}
-    
+    return {
+        'min': min(datums),
+        'max': max(datums),
+        'mean': "{0:.2f}".format(
+            sum(datums) / len(datums))}
+
+
 def plotter(data, field):
     if field not in data[0].keys():
         raise Exception("Fieldname not found.")
-        
+
     def convert(datum):
         time = datum['Time'].split(":")
-        time = float(time[0]) + float(time[1])/60.
+        time = float(time[0]) + float(time[1]) / 60.
         return time
-    
-    gp.plot((np.array([convert(datum) for datum in data]), np.array([float(x[field]) for x in data]), {'with': 'lines'}), terminal = 'dumb 200, 50', unset = 'grid', ascii=True, output="temp.txt", xlabel="Hour (24)", y2label="Speed (Mb/s)", set=("xtics 1", "ytics 5"), yrange=[0, 100])
+
+    gp.plot((np.array([convert(datum) for datum in data]),
+             np.array([float(x[field]) for x in data]),
+             {'with': 'lines'}),
+            terminal='dumb 200, 50',
+            unset='grid',
+            ascii=True,
+            output="temp.txt",
+            xlabel="Hour (24)",
+            y2label="Speed (Mb/s)",
+            set=("xtics 1",
+                 "ytics 5"),
+            yrange=[0,
+                    100])
     with open("temp.txt", "r") as fp:
         print fp.read()
     remove("temp.txt")
     return
+
 
 def uptime(date=1):
     today = datetime.date.today()
@@ -51,20 +69,35 @@ def uptime(date=1):
             if row['Date'] == yester:
                 results.append(row)
     return results
-    
+
+
 def main(date, plot):
     data = get_results(date)
     for cur in ['Download', 'Upload']:
         anal = analysis(data, cur)
         print "{}:\n\tAverage: {}\n\tMax: {}\n\tMin: {}".format(cur, anal['mean'], anal['max'], anal['min'])
-        if plot: plotter(data, cur)
+        if plot:
+            plotter(data, cur)
     ping = uptime(date)
     print "Total downtime: {} Minutes\nPercent Uptime: {}%".format(len(ping) - sum([int(x['Success']) for x in ping]), "{0:.2f}".format(float(sum([float(x['Success']) for x in ping])) / float(len(ping)) * 100))
     return
-    
+
+
 if __name__ == "__main__":
     option_parser = OptionParser(usage="usage: %prog [flags]")
-    option_parser.add_option('-d', '--date', action='store', dest='date', default=1, type="int", help='How many days back you want to run on the data.')
-    option_parser.add_option('-p', '--plot', action='store_true', dest='plot', help='Plot the upload and download speed over the course of the day.')
+    option_parser.add_option(
+        '-d',
+        '--date',
+        action='store',
+        dest='date',
+        default=1,
+        type="int",
+        help='How many days back you want to run on the data.')
+    option_parser.add_option(
+        '-p',
+        '--plot',
+        action='store_true',
+        dest='plot',
+        help='Plot the upload and download speed over the course of the day.')
     (options, args) = option_parser.parse_args()
     main(options.date, options.plot)
--- speed_tester.py
+++ speed_tester.py
@@ -1,18 +1,13 @@
 import os
-import sys
-import time
 from datetime import datetime
-import daemon
-import signal
-import threading
-import multiprocessing
 import twitter
-import json 
+import json
 import random
 from logger import Logger
 from optparse import OptionParser
 from speedtest import Speedtest
 from socket import setdefaulttimeout
+
 
 class PingTest():
     def __init__(self, numPings=3, pingTimeout=2, maxWaitTime=6):
@@ -20,26 +15,35 @@ class PingTest():
         self.pingTimeout = pingTimeout
         self.maxWaitTime = maxWaitTime
         self.config = json.load(open('./config.json'))
-        self.logger = Logger(self.config['log']['type'], { 'filename': self.config['log']['files']['ping'] })
+        self.logger = Logger(
+            self.config['log']['type'],
+            {'filename': self.config['log']['files']['ping']})
 
     def run(self):
         pingResults = self.doPingTest()
         self.logPingResults(pingResults)
 
     def doPingTest(self):
-        response = os.system("ping -c %s -W %s -w %s 8.8.8.8 > /dev/null 2>&1" % (self.numPings, (self.pingTimeout * 1000), self.maxWaitTime))
+        response = os.system(
+            "ping -c %s -W %s -w %s 8.8.8.8 > /dev/null 2>&1" %
+            (self.numPings, (self.pingTimeout * 1000), self.maxWaitTime))
         success = 0
         if response == 0:
             success = 1
-        return { 'date': datetime.now(), 'success': success }
+        return {'date': datetime.now(), 'success': success}
 
     def logPingResults(self, pingResults):
-        self.logger.log([ pingResults['date'].strftime('%Y-%m-%d'),pingResults['date'].strftime('%H:%M:%S'), str(pingResults['success'])])
+        self.logger.log([pingResults['date'].strftime('%Y-%m-%d'),
+                         pingResults['date'].strftime('%H:%M:%S'),
+                         str(pingResults['success'])])
+
 
 class SpeedTest():
     def __init__(self):
         self.config = json.load(open('./config.json'))
-        self.logger = Logger(self.config['log']['type'], { 'filename': self.config['log']['files']['speed'] })
+        self.logger = Logger(
+            self.config['log']['type'],
+            {'filename': self.config['log']['files']['speed']})
 
     def run(self):
         speedTestResults = self.doSpeedTest()
@@ -54,22 +58,30 @@ class SpeedTest():
             s.get_best_server()
             s.download()
             s.upload()
-        except:
-            return { 'date': datetime.now(), 'uploadResult': 0, 'downloadResult': 0, 'ping': -1 }
-        
-        
-		
+        except BaseException:
+            return {
+                'date': datetime.now(),
+                'uploadResult': 0,
+                'downloadResult': 0,
+                'ping': -1}
+
         results = s.results.dict()
         pingResult = results['ping']
-        downloadResult = float("{0:.2f}".format(results['download']/1e6))
-        uploadResult = float("{0:.2f}".format(results['upload']/1e6))
+        downloadResult = float("{0:.2f}".format(results['download'] / 1e6))
+        uploadResult = float("{0:.2f}".format(results['upload'] / 1e6))
 
-        
-        return { 'date': datetime.now(), 'uploadResult': uploadResult, 'downloadResult': downloadResult, 'ping': pingResult }
+        return {
+            'date': datetime.now(),
+            'uploadResult': uploadResult,
+            'downloadResult': downloadResult,
+            'ping': pingResult}
 
     def logSpeedTestResults(self, speedTestResults):
-        self.logger.log([ speedTestResults['date'].strftime('%Y-%m-%d'),  speedTestResults['date'].strftime('%H:%M:%S'), str(speedTestResults['uploadResult']), str(speedTestResults['downloadResult']), str(speedTestResults['ping']) ])
-
+        self.logger.log([speedTestResults['date'].strftime('%Y-%m-%d'),
+                         speedTestResults['date'].strftime('%H:%M:%S'),
+                         str(speedTestResults['uploadResult']),
+                         str(speedTestResults['downloadResult']),
+                         str(speedTestResults['ping'])])
 
     def tweetResults(self, speedTestResults):
         thresholdMessages = self.config['tweetThresholds']
@@ -77,28 +89,42 @@ class SpeedTest():
         for (threshold, messages) in thresholdMessages.items():
             threshold = float(threshold)
             if speedTestResults['downloadResult'] < threshold:
-                message = messages[random.randint(0, len(messages) - 1)].replace('{tweetTo}', self.config['tweetTo']).replace('{internetSpeed}', self.config['internetSpeed']).replace('{downloadResult}', str(speedTestResults['downloadResult']))
+                message = messages[random.randint(0,
+                                                  len(messages) - 1)].replace('{tweetTo}',
+                                                                              self.config['tweetTo']).replace('{internetSpeed}',
+                                                                                                              self.config['internetSpeed']).replace('{downloadResult}',
+                                                                                                                                                    str(speedTestResults['downloadResult']))
 
         if message:
-            api = twitter.Api(consumer_key=self.config['twitter']['twitterConsumerKey'],
-                            consumer_secret=self.config['twitter']['twitterConsumerSecret'],
-                            access_token_key=self.config['twitter']['twitterToken'],
-                            access_token_secret=self.config['twitter']['twitterTokenSecret'])
+            api = twitter.Api(
+                consumer_key=self.config['twitter']['twitterConsumerKey'],
+                consumer_secret=self.config['twitter']['twitterConsumerSecret'],
+                access_token_key=self.config['twitter']['twitterToken'],
+                access_token_secret=self.config['twitter']['twitterTokenSecret'])
             if api:
-                status = api.PostUpdate(message)
-                
+                api.PostUpdate(message)
+
+
 if __name__ == "__main__":
     option_parser = OptionParser(usage="usage: %prog [flags]")
-    option_parser.add_option('-p', '--ping', action='store_true', dest='ping', help='Do a ping test.')
-    option_parser.add_option('-s', '--speed', action='store_true', dest='speed', help='Do a speed test')
-    
+    option_parser.add_option(
+        '-p',
+        '--ping',
+        action='store_true',
+        dest='ping',
+        help='Do a ping test.')
+    option_parser.add_option(
+        '-s',
+        '--speed',
+        action='store_true',
+        dest='speed',
+        help='Do a speed test')
+
     (options, args) = option_parser.parse_args()
-    
+
     if options.ping:
         pinger = PingTest()
         pinger.run()
     if options.speed:
         speeder = SpeedTest()
         speeder.run()
-                
-                
